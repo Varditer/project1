1daf9bb06462895fd86fecbc0559a2933753143e
Varditer <Varditertovmasyan@gmail.com>
2020-11-06 23:56:05 +0400

    chang

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -34,13 +34,14 @@ public class Main {
             }
             System.out.print(array[i]);
         }
-
+        System.out.print("TASK 5 Create an array and fill it. Print all elements which are between 24.12 and 467.23.");
         float[]array6 ={25,2f, 29.52f ,467.23f,17,24.20f,427.90f, 46.9f, 22.3f, 123.4f };
         for (int j = 0; j < array6.length ; j++) {
             if (array6[j] > 24.12 &&  array6[j] < 467.23){
                System.out.println(array6[j]);
             }
         }
+        System.out.print("Task 3 Create an array and fill it with odd numbers from -20:20");
         int[] array3 = new int[20];
         int x = -19;
         for (int i = 0; i < array3.length; i++) {
@@ -48,6 +49,12 @@ public class Main {
           x=x+2;
           System.out.println(array3[i]);
         }
+        System.out.print("Task 7 Given an integer, 0< N < 21 , print its first\n" +
+                "10 multiples.\n" +
+                "\n" +
+                "Each multiple N x i (where 0<i<11)\n" +
+                "should be printed on a new line in the\n" +
+                "form: N x i = result.");
         int y = 2;
         for (int i = 1; i<=10; i++) {
             int z = i * y;
